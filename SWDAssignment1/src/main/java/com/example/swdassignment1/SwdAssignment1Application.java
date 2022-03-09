dcf64e1c56795b934199f962c93abbfea10d6d00
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2022-03-09 15:32:52 +0700

    Add files via upload

---
+++ SWDAssignment1/src/main/java/com/example/swdassignment1/SwdAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.swdassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwdAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwdAssignment1Application.class, args);
+    }
+
+}
---
+++ SWDAssignment1/src/test/java/com/example/swdassignment1/SwdAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swdassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwdAssignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
